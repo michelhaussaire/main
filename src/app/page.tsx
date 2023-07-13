861724aac72fa98f07c9d368029545f6c43d2ffc
Michel <haussairemichel@gmail.com>
2023-07-13 17:59:55 -0300

    cambio en paginas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,18 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>hola</h1>
+      <header>
+        <h1>Michel Haussaire</h1>
+        <p>Soy desarrollador web</p>
+      </header>
+      <body>
+        <h2>Info y Tecnologias</h2>
+        <h2>Proyectos</h2>
+        <h2>Contacto</h2>
+      </body>
+      <footer>
+        <p>Redes</p>
+      </footer>
     </main>
   );
 }
