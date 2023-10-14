c6b5c4482fbd860a5a32a9e9b0a464bb1fe94242
Michel Haussaire <haussairemichel@gmail.com>
2023-10-14 13:17:59 -0300

    Componentes a src

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 import Image from "next/image";
-import Header from "../../components/Header";
-import InfoTec from "../../components/InfoTec";
-import Proyectos from "../../components/Proyectos";
-import Contacto from "../../components/Contacto";
-import Footer from "../../components/Footer";
+import Header from "../components/Header";
+import InfoTec from "../components/InfoTec";
+import Proyectos from "../components/Proyectos";
+import Contacto from "../components/Contacto";
+import Footer from "../components/Footer";
 
 export default function Home() {
   return (
